c0562739e5e171367286d485b31b40280b90b8f3
Arthur Bressan <arthurbressan2@hotmail.com>
2014-07-20 10:22:36 -0300

    Update tags to 1.0.0

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 setup(
     name='fixedwidthwriter',
     packages=['fixedwidthwriter'],  # this must be the same as the name above
-    version='0.0.3',
+    version='1.0.0',
     description='A class to write fixed-width files with an interface similar to python`s csv writer',
     author='Arthur Bressan',
     author_email='arthurpbressan@gmail.com',
     url='https://github.com/HardDiskD/py-fixedwidthwriter',  # use the URL to the github repo
-    download_url='https://github.com/HardDiskD/py-fixedwidthwriter/tarball/0.0.1', # I'll explain this in a second
+    download_url='https://github.com/HardDiskD/py-fixedwidthwriter/tarball/1.0.0',
     keywords=['file writer', 'fixed width'],  # arbitrary keywords
     classifiers=[],
 )
